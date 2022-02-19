4c078af6981184ebf82f0e84fa82d511ed3ce91e
knight196 <71406824+knight196@users.noreply.github.com>
2022-02-19 18:12:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,30 @@ function scrollFunction() {
     }
 }
 
+//tabs page
+function Tabspage(evt, Tabsname) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Tabsname).style.display = "block";
+    evt.currentTarget.className += " active";
+  } 
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 //anime page collection
 //list of animes are shown in the container from the api
 const api = 'https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1';
